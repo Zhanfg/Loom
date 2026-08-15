260f4087c9fd4e44f08bebe6e47499bd10f0a822
Axymorrsen <Axymosen@outlook.com>
2026-08-15 21:51:41 +0800

    stage23: add multi big-pcluster oracle proof to hard gate

--- tests/ci/linux-hard-gate.sh
+++ tests/ci/linux-hard-gate.sh
@@ -37,5 +37,6 @@ bash tests/integration/stage19_erofs_big_pcluster_oracle.sh
 bash tests/integration/stage20_erofs_big_pcluster_self_encode.sh
 bash tests/integration/stage21_erofs_big_pcluster_core_unification.sh
 bash tests/integration/stage22_erofs_variable_cblkcnt.sh
+bash tests/integration/stage23_erofs_multi_big_oracle.sh
 
 printf '%s\n' 'Loom Linux filesystem hard gate PASS'
